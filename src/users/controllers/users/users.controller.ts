74470af6d3ef7818f4a6736d832a164e81f4329b
Vestertech <vesteerwood@gmail.com>
2024-04-04 00:46:18 +0100

    Added some documentation to the code base

--- src/users/controllers/users/users.controller.ts
+++ src/users/controllers/users/users.controller.ts
@@ -16,6 +16,7 @@ import { UsersService } from 'src/users/services/users/users.service';
 
 @Controller('users')
 export class UsersController {
+  // const initializes controller with injected usersService
   constructor(private userService: UsersService) {}
   @Get()
   getUsers() {
@@ -23,25 +24,31 @@ export class UsersController {
   }
 
   @Post()
+  // Extracts data from the request body using @Body() decorator.
+  // Calls createUser() method of the UsersService to create a new user in the database.
   createUser(@Body() createUserDto: CreateUserDto) {
     return this.userService.createUser(createUserDto);
   }
   @Put(':id')
   async updateUserById(
+    // Extracts userID & userData from req. using param and Body
     @Param('id', ParseIntPipe) id: number,
     @Body() updateUserDto: UpdateUserDto,
   ) {
+    // Calls updateUser() method of the UsersService to update the user in the database.
     await this.userService.updateUser(id, updateUserDto);
   }
 
   @Delete(':id')
   async DeleteUserById(@Param('id', ParseIntPipe) id: number) {
+    // Calls deleteUser() method of the UsersService to delete the user from the database.
     await this.userService.deleteUser(id);
   }
 
   @Post(':id/profiles')
   createUserProfile(
     @Param('id', ParseIntPipe) id: number,
+    // Extracts profile data from the request body using @Body() decorator.
     @Body() createUserProfileDto: CreateUserProfileDto,
   ) {
     return this.userService.createUserProfile(id, createUserProfileDto);
@@ -54,4 +61,11 @@ export class UsersController {
   ) {
     return this.userService.createUserPost(id, createUserPostDto);
   }
+
+  // createUserPost
+
+  // Handles HTTP POST requests to create a new post for a user.
+  // Extracts user ID from the request parameters using @Param() decorator.
+  // Extracts post data from the request body using @Body() decorator.
+  // Calls createUserPost() method of the UsersService to create a new post for the user in the database.
 }
--- src/users/services/users/users.service.ts
+++ src/users/services/users/users.service.ts
@@ -14,45 +14,55 @@ import {
 @Injectable()
 export class UsersService {
   constructor(
+    // @injectRepository used for injecting TypeORM repositories for User, Profile & Post into the service.
     @InjectRepository(User) private userRepository: Repository<User>,
     @InjectRepository(Profile) private profileRepository: Repository<Profile>,
     @InjectRepository(Post) private postRepository: Repository<Post>,
   ) {}
   findUsers() {
+    // Retrieves all users from the database along with their associated profiles and posts.
+    // Uses userRepository.find() with relations option to include associated entities (profile and posts).
     return this.userRepository.find({
-      relations: [
-        'profile',
-        'posts',
-        // exact field name in typeorm entity
-      ],
+      relations: ['profile', 'posts'],
     });
   }
   createUser(userDetails: CreateUserParams) {
     const newUser = this.userRepository.create({
       ...userDetails,
+      // Sets createdAt to current date
       createdAt: new Date(),
     });
+    // Saves the new user to the database using userRepository.save().
     return this.userRepository.save(newUser);
   }
+  // Updates the user with the provided ID with new details.
   updateUser(id: number, updateUserDetails: UpdateUserParams) {
+    // Uses userRepository.update() to update the user entity with matching ID.
     return this.userRepository.update({ id }, { ...updateUserDetails });
   }
   deleteUser(id: number) {
+    // Uses userRepository.delete() to delete the user entity with matching ID.
     return this.userRepository.delete({ id });
   }
 
   async createUserProfile(
+    // Creates a new profile for the user with the provided ID.
     id: number,
     createUserProfileDetails: CreateUserProfileParams,
   ) {
+    // Retrieves the user entity by ID using userRepository.findOne().
     const user = await this.userRepository.findOneBy({ id });
+    // If the user is not found, throws an HTTP exception.
     if (!user)
       throw new HttpException(
         'User not found, Cannot create Profile',
         HttpStatus.BAD_REQUEST,
       );
+    // Creating new profile
     const newProfile = this.profileRepository.create(createUserProfileDetails);
+    // Saving profile to DB
     const savedProfile = await this.profileRepository.save(newProfile);
+    // Associating profile with user and saves user entity
     user.profile = savedProfile;
     return this.userRepository.save(user);
   }
@@ -62,6 +72,7 @@ export class UsersService {
     createUserPostDetails: CreateUserPostParams,
   ) {
     // Create post, save it and attach to a user.
+    // retrieving user entity by .findOne
     const user = await this.userRepository.findOneBy({ id });
     if (!user)
       throw new HttpException(
@@ -69,6 +80,7 @@ export class UsersService {
         HttpStatus.BAD_REQUEST,
       );
     const newPost = this.postRepository.create({
+      // associating new post with user
       ...createUserPostDetails,
       user,
     });
